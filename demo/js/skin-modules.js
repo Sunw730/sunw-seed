906d9cf3169101fce0c0d3f07132b9aede129029
SunWang <sunwang_730@163.com>
2015-09-28 15:47:04 +0800

    支持dom节点自定义属性，提交动态加载皮肤示例代码

---
+++ demo/js/skin-modules.js
@@ -0,0 +1,6 @@
+/**
+ * Created by [Sun Wang] on 2015/9/16.
+ */
+
+Sunw.add("blue", {url: "../demo/css/blue.css", extra: {skin: "blue"}})
+    .add("green", {url: "../demo/css/green.css", extra: {skin: "green"}})
